f226751470a5e4ba79e697cc5c68dcfb37690825
wojtek717 <wojciechkonury@gmail.com>
2019-09-22 12:31:35 +0200

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MagicBall());
+
+class MagicBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[800],
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: FlatButton(
+                onPressed: (){
+                  print('Clicked');
+                },
+                highlightColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                child: Image(
+                  image: AssetImage('images/ball1.png'),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
